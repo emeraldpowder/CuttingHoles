215cfa7781e60974a847de3e6ae65c85384e96a4
Glavak <gau-gau@ya.ru>
2019-07-26 16:50:05 +0700

    Added project & readme

---
+++ Assets/HoleTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class HoleTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.layer = 9;
+        Debug.Log("Enter");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.gameObject.layer = 0;
+        Debug.Log("Exit");
+    }
+}
